8d864a0d7d4845c84eac7bd2ab2d085a51b3d6ed
Setayesh Karampoor <setayeshkarampoor623@gmail.com>
2025-12-12 02:34:34 +0330

    Add Page Shop

--- assets/js/particles-shop.js
+++ assets/js/particles-shop.js
@@ -1,49 +1,30 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
-
-/* Otherwise just put the config content (json): */
-
 particlesJS(
   "particles-js",
 
   {
     fullScreen: { enable: false },
     particles: {
-      number: { value: 1 }, // فقط یه ذره!
+      number: { value: 3 },
       color: {
         value: [
-          "#5e00ff",
-          "#8a2be2",
-          "#ff00ff",
-          "#ff6b6b",
-          "#ffd93d",
           "#1a0033",
           "#2b0066",
           "#3f00b5",
-          "#5e00ff",
         ],
         animation: {
           enable: true,
-          speed: 30,
+          speed: 40,
           sync: false,
         },
       },
       shape: { type: "circle" },
-      opacity: { value: 0.1 },
+      opacity: { value: 0.3 },
       size: {
-        value: 300,
+        value: 150,
         random: false,
         animation: {
           enable: true,
-          speed: 40,
+          speed: 30,
           minimumValue: 600,
           maximumValue: 1000,
           sync: false,
@@ -57,6 +38,9 @@ particlesJS(
         straight: false,
         outModes: "bounce",
         attract: { enable: false },
+      },
+      line_linked: {
+        enable: false,
       },
       wobble: { enable: true, distance: 50, speed: 10 },
       links: { enable: false },
